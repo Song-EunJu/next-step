21147b6ef1c37f5c766aeb2bf493704f2a3d77c0
Song-EunJu <jd06280@naver.com>
2024-01-11 23:59:00 +0900

    add : 기본 구분자/커스텀 구분자 존재여부 판별 메서드 분리 및 테스트코드 추가

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,20 +2,30 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public static void main(String[] args) {
-        String testStr = "//;\n1;2;3";
-
-        String regex = "//[^\n]*\n";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(testStr);
-        if(matcher.find()){
+    public int calculate(String target) {
+        if(hasCustomDelimiter(target)){
             // 커스텀 구분자 패턴이 있는 경우
         }
         else {
             // 기본 구분자가 있는 경우
-            if(testStr.contains(":") || testStr.contains(",")){
-
-            }
         }
+        return 0;
+    }
+
+    // 커스텀 구분자 패턴 존재 여부 판별 메서드
+    public boolean hasCustomDelimiter(String target) {
+        String customDelimiterRegex = "//[^\n]*\n";
+        Pattern pattern = Pattern.compile(customDelimiterRegex);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+    // 기본 구분자 존재 여부 판별 메서드
+    public boolean hasDefaultDelimiter(String target) {
+        String defaultDelimiterRegex = "[:,]";
+//        Pattern pattern = Pattern.compile();
+//        Matcher matcher = pattern.matcher(target);
+//        return matcher.find();
+        return true;
     }
 }
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,9 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @Before
+    public void setup(){
+        calculator = new StringCalculator();
+    }
 
     @Test
-    public void hi(){
-        System.out.println("hi");
+    @DisplayName("기본 구분자 쉼표 (,) 와 콜론 (:) 으로 구분된 숫자의 합을 반환한다.")
+    public void separatedByDefaultDelimiter() {
+        assertEquals(6, calculator.calculate("1,2,3"));
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자로 구분된 숫자의 합을 반환한다.")
+    public void separatedByCustomDelimiter(){
+        assertEquals(6, calculator.calculate("//;\n1;2;3"));
     }
 }
