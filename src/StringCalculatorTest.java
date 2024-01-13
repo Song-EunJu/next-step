75cbaae846f35fa65a0130a229fd4b785b64b9c2
Song-EunJu <jd06280@naver.com>
2024-01-13 14:18:40 +0900

    add : 기본 요구사항 (음수 제외) 만족하는 테스트코드 추가

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
     private StringCalculator calculator;
+    private static final String DEFAULT_DELIMITER = "[:,]";
 
     @Before
     public void setup(){
@@ -13,14 +14,28 @@ public class StringCalculatorTest {
     }
 
     @Test
-    @DisplayName("기본 구분자 쉼표 (,) 와 콜론 (:) 으로 구분된 숫자의 합을 반환한다.")
+    @DisplayName("기본 구분자 쉼표 (,) 로 구분된 숫자의 합을 반환한다.")
     public void separatedByDefaultDelimiter() {
-        assertEquals(6, calculator.calculate("1,2,3"));
+        assertEquals(6, calculator.sumSplitByDefaultDelimiter(",", "1,2,3"));
+    }
+
+    @Test
+    @DisplayName("기본 구분자 쉼표 (,) 와 콜론 (:) 으로 구분된 숫자의 합을 반환한다.")
+    public void separatedByDefaultDelimiters() {
+        assertEquals(6, calculator.sumSplitByDefaultDelimiter(DEFAULT_DELIMITER, "1,2:3"));
+    }
+
+    @Test
+    @DisplayName("구분자가 없는 문자열에서, 숫자의 합을 반환한다.")
+    public void nonDelimiter() {
+        assertEquals(0, calculator.sumSplitByNoneDelimiter(""));
+        assertEquals(0, calculator.sumSplitByNoneDelimiter(" "));
+        assertEquals(100, calculator.sumSplitByNoneDelimiter("100"));
     }
 
     @Test
     @DisplayName("커스텀 구분자로 구분된 숫자의 합을 반환한다.")
     public void separatedByCustomDelimiter(){
-        assertEquals(6, calculator.calculate("//;\n1;2;3"));
+        assertEquals(6, calculator.sumSplitByCustomDelimiter("//;\n1;2;3"));
     }
 }
