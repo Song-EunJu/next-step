2b15ac26f0bac84be9b2567f48ea01f5b8365a19
Song-EunJu <jd06280@naver.com>
2024-01-13 14:19:49 +0900

    mod : 기본 구분자, 커스텀 구분자 기준 문자열 합 리턴 메서드 분리

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,58 +2,33 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public int calculate(String target) {
-        // 커스텀 구분자도 있고, 기본 구분자도 있는 경우
-        if(hasCustomDelimiter(target) && hasDefaultDelimiter(target)) {
-
-        }
-
-        // 커스텀 구분자만 있는 경우
-        if(hasCustomDelimiter(target)) {
-            // 1개만 있을 경우
-
-            // 2개 이상일 경우
-        }
-
-        // 기본 구분자만 있는 경우
-        if(hasDefaultDelimiter(target)) {
-
-        }
-
-        // 구분자가 없는 경우 (커스텀 구분자, 기본 구분자 둘다 X)
-        if(!(hasCustomDelimiter(target) || hasDefaultDelimiter(target))) {
-            return target.isBlank() ? 0 : Integer.valueOf(target);
-        }
-        return 0;
+    public int sumSplitByNoneDelimiter(String target) {
+        return target.isBlank() ? 0 : Integer.valueOf(target);
     }
 
-    // 커스텀 구분자가 여러 개인 경우를 판별
-    public boolean hasCustomDelimiters(String target) {
-        String customDelimiterRegex = "//[^\n]*\n";
-
-        Pattern pattern = Pattern.compile(customDelimiterRegex);
-        Matcher matcher = pattern.matcher(target);
-
-        while(matcher.find()) {
-            String found = matcher.group();
-            System.out.println("Found: " + found);
+    public int sumSplitByDefaultDelimiter(String delimiter, String target) {
+        int sum = 0;
+        String[] targetNumbers = target.split(delimiter);
+        for (String number : targetNumbers) {
+            sum += Integer.parseInt(number);
         }
-        return matcher.find();
+        return sum;
     }
 
-    // 커스텀 구분자 패턴 존재 여부 판별 메서드
-    public boolean hasCustomDelimiter(String target) {
-        String customDelimiterRegex = "//[^\n]*\n";
-        Pattern pattern = Pattern.compile(customDelimiterRegex);
-        Matcher matcher = pattern.matcher(target);
-        return matcher.matches();
-    }
+    public int sumSplitByCustomDelimiter(String target) {
+        String pattern = "//(.*?)\n";
 
-    // 기본 구분자 존재 여부 판별 메서드
-    public boolean hasDefaultDelimiter(String target) {
-        String defaultDelimiterRegex = "[:,]";
-        Pattern pattern = Pattern.compile(defaultDelimiterRegex);
-        Matcher matcher = pattern.matcher(target);
-        return matcher.matches();
+        // 패턴과 입력 문자열을 사용하여 매칭 수행
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(target);
+
+        // 매칭된 결과 출력
+        String customDelimiter = "";
+        while (matcher.find()) {
+            customDelimiter = matcher.group(1);
+        }
+
+        String targetExceptDelimiter = target.substring(target.indexOf("\n")+1);
+        return sumSplitByDefaultDelimiter(customDelimiter, targetExceptDelimiter);
     }
 }
