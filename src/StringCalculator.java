b87ab36bbfe86d335b755bdb908b907b55b9060c
Song-EunJu <jd06280@naver.com>
2024-01-14 23:09:26 +0900

    mod : 커스텀 구분자 추출 및 이후 문자열 반환 메서드 분리

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,33 +2,58 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
-    public int sumSplitByNoneDelimiter(String target) {
+    private static final String CUSTOM_DELIMITER = "//(.*?)\n";
+
+    // 구분자가 없는 경우
+    public int getNumber(String target) {
         return target.isBlank() ? 0 : Integer.valueOf(target);
     }
 
+    // 기본 구분자로 계산하는 경우
     public int sumSplitByDefaultDelimiter(String delimiter, String target) {
         int sum = 0;
         String[] targetNumbers = target.split(delimiter);
         for (String number : targetNumbers) {
-            sum += Integer.parseInt(number);
+            sum += getNumber(number);
         }
         return sum;
     }
 
+    // 커스텀 구분자로 계산하는 경우
     public int sumSplitByCustomDelimiter(String target) {
-        String pattern = "//(.*?)\n";
+        String customDelimiter = getCustomDelimiter(target);
 
-        // 패턴과 입력 문자열을 사용하여 매칭 수행
-        Pattern regex = Pattern.compile(pattern);
+        String targetStr = target.substring(target.indexOf("\n")+1);
+        return sumSplitByDefaultDelimiter(customDelimiter, targetStr);
+    }
+
+    // 커스텀 구분자 뽑아내기
+    public String getCustomDelimiter(String target) {
+        Pattern regex = Pattern.compile(CUSTOM_DELIMITER);
         Matcher matcher = regex.matcher(target);
 
-        // 매칭된 결과 출력
         String customDelimiter = "";
         while (matcher.find()) {
             customDelimiter = matcher.group(1);
         }
-
-        String targetExceptDelimiter = target.substring(target.indexOf("\n")+1);
-        return sumSplitByDefaultDelimiter(customDelimiter, targetExceptDelimiter);
+        return customDelimiter;
     }
+
+    // 커스텀 구분자 이후 문자열 반환
+    public String getTargetStrExceptCustomDelimiter(String target){
+        return target.substring(target.indexOf("\n")+1);
+    }
+
+    // 음수 판별
+//    public boolean checkNegativeValue(String target) {
+//        // 커스텀 구분자에 - 가 없는데, 이후에 - 가 등장하는 경우
+//        if(!getCustomDelimiter(target).equals("-") && getTargetStrExceptCustomDelimiter(target).contains("-")) {
+//
+//        }
+//                return true;
+//            return false;
+//        }
+//        // 커스텀 구분자에 - 가 있는 경우 -> 무조건 성립
+//    }
+//
 }
