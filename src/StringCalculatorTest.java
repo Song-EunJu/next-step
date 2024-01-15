613afa1cbbafb48ae46e5881a7b5baa76ccfc143
Song-EunJu <jd06280@naver.com>
2024-01-15 22:36:39 +0900

    add : 기본구분자, 커스텀 구분자도 아닌 구분자 나올 경우 예외처리 테스트 코드 추가

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,12 +1,12 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.Assert.assertEquals;
 
 public class StringCalculatorTest {
     private StringCalculator calculator;
-    private static final String DEFAULT_DELIMITER = "[:,]";
 
     @Before
     public void setup(){
@@ -16,36 +16,44 @@ public class StringCalculatorTest {
     @Test
     @DisplayName("구분자가 없는 문자열에서, 숫자의 합을 반환한다.")
     public void nonDelimiter() {
-        assertEquals(0, calculator.getNumber(""));
-        assertEquals(0, calculator.getNumber(" "));
-        assertEquals(100, calculator.getNumber("100"));
+        assertEquals(0, calculator.add(""));
+        assertEquals(0, calculator.add(" "));
+        assertEquals(100, calculator.add("100"));
     }
 
     @Test
     @DisplayName("기본 구분자 쉼표 (,) 로 구분된 숫자의 합을 반환한다.")
     public void separatedByDefaultDelimiter() {
-        assertEquals(6, calculator.sumSplitByDefaultDelimiter(",", "1,2,3"));
-        assertEquals(5, calculator.sumSplitByDefaultDelimiter(",", ",2,3"));
-        assertEquals(5, calculator.sumSplitByDefaultDelimiter(",", "2,,3"));
+        assertEquals(6, calculator.add("1,2,3"));
+        assertEquals(5, calculator.add(",2,3"));
+        assertEquals(5, calculator.add("2,,3"));
     }
 
     @Test
     @DisplayName("기본 구분자 쉼표 (,) 와 콜론 (:) 으로 구분된 숫자의 합을 반환한다.")
     public void separatedByDefaultDelimiters() {
-        assertEquals(6, calculator.sumSplitByDefaultDelimiter(DEFAULT_DELIMITER, "1,2:3"));
+        assertEquals(6, calculator.add("1,2:3"));
     }
 
     @Test
     @DisplayName("커스텀 구분자로 구분된 숫자의 합을 반환한다.")
     public void separatedByCustomDelimiter(){
-        assertEquals(6, calculator.sumSplitByCustomDelimiter("//;\n1;2;3"));
+        assertEquals(6, calculator.add("//;\n1;2;3"));
     }
 
     // TODO : 커스텀 구분자와 기본 구분자가 섞여있는 경우
 
     @Test
-    @DisplayName("문자열에 음수가 있는 경우, 예외 처리한다.")
-    public void checkNegativeValue(){
-        assertEquals("음수는 존재할 수 없습니다", calculator.sumSplitByCustomDelimiter("//;\n1;2;3"));
+    @DisplayName("기본 구분자도 아니고, 커스텀 구분자도 아닌 구분자가 나온 경우 예외 처리한다.")
+    public void separatedByExceptionDelimiter(){
+        Assertions.assertThrows(RuntimeException.class, () ->
+            calculator.add("//#\n1;2#3")
+        );
     }
+
+//    @Test
+//    @DisplayName("문자열에 음수가 있는 경우, 예외 처리한다.")
+//    public void checkNegativeValue(){
+//        assertEquals("음수는 존재할 수 없습니다", calculator.add("//#\\n1;2#3"));
+//    }
 }
